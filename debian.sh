74addc39b75a05ce2fda26c675ee162fba0a1382
maninder-mike <maninder.singh@outlook.in>
2021-02-20 22:03:45 +0530

    find uname -n

--- debian.sh
+++ debian.sh
@@ -1,3 +1,4 @@
 echo "You are in Debian file."
 
+
 sudo -S apt install fish terminator sqlite3 sqlitebrowser python3 python3-pip git nodejs npm tlp tlp-rdw gnome-tweak-tool ubuntu-restricted-extras neovim vifm 
--- easyinstall.sh
+++ easyinstall.sh
@@ -4,13 +4,17 @@ VER=0.1.0
 PROJECT_NAME="easyinstall"
 PROJECT="https://github.com/Maninder-mike/$PROJECT_NAME"
 
+Node_name=$(uname -n)
+echo $Node_name
+
 function _help() {
     # echo "this is EasyInstall for single command install essential app for your system: "
     # echo $PROJECT_NAME
     # echo $VER
     # echo $PROJECT
     # _arch
-    _debian
+    # _debian
+    . test.sh
 }
 
 function _arch() {
