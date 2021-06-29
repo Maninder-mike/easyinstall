0ac9180410f5a955ab6f202bd7743b8acfd4b60b
Maninder-mike <maninder.singh@outlook.in>
2021-06-29 12:15:31 +0530

    add apps

--- easyinstall.sh
+++ easyinstall.sh
@@ -12,14 +12,20 @@ function _help() {
     # echo $PROJECT_NAME
     # echo $VER
     # echo $PROJECT
-    
-    # if [$Node_name = "fedora"];then
-    # echo "this run fedora" 
-    # fi
 
+    # if [$Node_name = "fedora"];then
+    # echo "this run fedora"
+    # fi
     case $Node_name in
     "fedora")
-    fedora;
+        _fedora
+        ;;
+    "debian")
+        _debian
+        ;;
+    "arch")
+        _arch
+        ;;
     esac
 
     # _arch
@@ -29,30 +35,28 @@ function _help() {
 
 function _arch() {
 
-    sudo pacman -Syyuu && \
-
-    sudo -S pacman -S neovim vifm pulseaudio pamixer htop \
-    tlp tlp-rdw numlockx base-devel xscreensaver firefox cups \
-    git go nodejs npm nautilus evince nomacs system-config-printer \
-    vlc sqlite sqlitebrowser networkmanager 
+    sudo pacman -Syyuu &&
+        sudo -S pacman -S neovim vifm pulseaudio pamixer htop \
+            tlp tlp-rdw numlockx base-devel xscreensaver firefox cups \
+            git go nodejs npm nautilus evince nomacs system-config-printer \
+            vlc sqlite sqlitebrowser networkmanager
 }
 
 function _debian() {
     sudo -S apt install fish terminator sqlite3 sqlitebrowser \
-    python3 python3-pip git nodejs npm tlp tlp-rdw gnome-tweak-tool \
-    ubuntu-restricted-extras neovim vifm 
+        python3 python3-pip git nodejs npm tlp tlp-rdw gnome-tweak-tool \
+        ubuntu-restricted-extras neovim vifm
 }
 
-
-function fedora() {
-    sudo dnf update && \
-    sudo rpm -Uvh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm || \
-    sudo rpm -Uvh http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm || \
-    sudo dnf install git nodejs npm fish vlc sqlite sqlitebrowser tlp tlp-rdw && \
-    sudo systemctl enable tlp && \
-    sudo npm i yarn -g && \
-    sudo chsh -s /usr/bin/fish && \
-    sudo dnf autoremove
+function _fedora() {
+    sudo dnf update &&
+        sudo rpm -Uvh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm ||
+        sudo rpm -Uvh http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm ||
+        sudo dnf install git nodejs npm fish vlc sqlite sqlitebrowser tlp tlp-rdw golang &&
+        sudo systemctl enable tlp &&
+        sudo npm i yarn -g &&
+        sudo chsh -s /usr/bin/fish && chsh -s /usr/bin/fish &&
+        sudo dnf autoremove
 }
 
 _help
