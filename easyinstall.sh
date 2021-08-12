9604e3ce6c030058d7fb1d00022693e1b399ef85
ਮਨਿੰਦਰ <maninder.singh@outlook.in>
2021-08-12 20:48:31 +0530

    line sink

--- easyinstall.sh
+++ easyinstall.sh
@@ -37,8 +37,7 @@ function _arch() {
 
 function _debian() {
     sudo -S apt install fish terminator sqlite3 sqlitebrowser \
-        python3 python3-pip git nodejs npm tlp tlp-rdw gnome-tweak-tool \
-        ubuntu-restricted-extras neovim vifm
+        python3 python3-pip git nodejs npm tlp tlp-rdw gnome-tweak-tool ubuntu-restricted-extras neovim vifm
 }
 
 function _fedora() {
